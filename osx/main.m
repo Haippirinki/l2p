13ea07133ea7e4f074bd8f1345e9d55c762fd42b
Jaakko Väyrynen <jaakko.vayrynen@iki.fi>
2017-04-13 19:17:51 +0300

    Added an OpenGL context.

--- osx/main.m
+++ osx/main.m
@@ -1,4 +1,6 @@
 #include <AppKit/AppKit.h>
+#include <OpenGL/OpenGL.h>
+#include <OpenGL/gl3.h>
 
 int main(int argc, char** argv)
 {
@@ -12,6 +14,21 @@ int main(int argc, char** argv)
 	[window makeKeyAndOrderFront:nil];
 
 	[NSApp activateIgnoringOtherApps:YES];
+
+	NSOpenGLPixelFormatAttribute attributes[] = {
+		NSOpenGLPFADoubleBuffer,
+		NSOpenGLPFAOpenGLProfile, NSOpenGLProfileVersion4_1Core,
+		0
+	};
+	id pixelFormat = [[NSOpenGLPixelFormat alloc] initWithAttributes:attributes];
+	id context = [[NSOpenGLContext alloc] initWithFormat:pixelFormat shareContext:nil];
+	[context setView:[window contentView]];
+
+	[context makeCurrentContext];
+
+	glClearColor(1.f, 0.f, 1.f, 1.f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	[context flushBuffer];
 
 	[NSApp run];
 
