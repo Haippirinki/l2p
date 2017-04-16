1f5f94ce1cf9a1a3fefe7e3d86918052ace1ce39
Jaakko Väyrynen <jaakko.vayrynen@iki.fi>
2017-04-16 14:15:29 +0300

    Update the OpenGL context when the window resizes or moves.

--- osx/main.mm
+++ osx/main.mm
@@ -18,6 +18,16 @@ bool g_shouldExit = false;
 	g_shouldExit = true;
 }
 
+- (void)windowDidResize:(NSNotification*)notification
+{
+	[[NSOpenGLContext currentContext] update];
+}
+
+- (void)windowDidMove:(NSNotification*)notification
+{
+	[[NSOpenGLContext currentContext] update];
+}
+
 @end
 
 @interface WindowView : NSView
@@ -52,9 +62,9 @@ int main(int argc, char** argv)
 	[NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
 	[NSApp finishLaunching];
 
-	NSRect contentRect = NSMakeRect(0, 0, 1280, 720);
+	NSRect contentRect = NSMakeRect(0, 0, 600, 600);
 	int styleMask = NSWindowStyleMaskTitled | NSWindowStyleMaskClosable | NSWindowStyleMaskMiniaturizable | NSWindowStyleMaskResizable;
-	NSWindow* window = [[NSWindow alloc] initWithContentRect:contentRect styleMask:styleMask backing:NSBackingStoreRetained defer:NO];
+	NSWindow* window = [[NSWindow alloc] initWithContentRect:contentRect styleMask:styleMask backing:NSBackingStoreBuffered defer:NO];
 	[window makeKeyAndOrderFront:nil];
 
 	[NSApp activateIgnoringOtherApps:YES];
