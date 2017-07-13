21da557557a0453797be8257d2fa9e38379459c7
Jaakko Väyrynen <jaakko.vayrynen@iki.fi>
2017-07-13 15:59:56 +0300

    Set fullscreen flags if on api 19 or newer.

--- android/app/src/main/java/fi/haippirinki/l2p/MainActivity.java
+++ android/app/src/main/java/fi/haippirinki/l2p/MainActivity.java
@@ -1,4 +1,20 @@
 package fi.haippirinki.l2p;
 
+import android.os.Build;
+import android.view.View;
+
 public class MainActivity extends android.app.NativeActivity {
+	protected void onCreate(android.os.Bundle savedInstanceState) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                View.SYSTEM_UI_FLAG_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+
+        super.onCreate(savedInstanceState);
+    }
 }
