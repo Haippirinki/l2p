593e0f71354d472c6dbeb9005a7d539b35870235
Matias K <matias@grandcrugames.com>
2017-10-12 13:35:48 +0300

    default enemy type

--- levels/gen.py
+++ levels/gen.py
@@ -1,12 +1,12 @@
 def lerp(value_1, value_2, t):
 	return value_1 * (1.0 - t) + value_2 * t
 
-def arc(time, n, radius_1, radius_2, angle_1, angle_2, speed):
+def arc(time, n, radius_1, radius_2, angle_1, angle_2, speed, e_type = "basic"):
 	for i in range(n):
 		t = float(i) / float(n - 1)
 		radius = lerp(radius_1, radius_2, t)
 		angle = lerp(angle_1, angle_2, t)
-		print "basic", time, radius, angle, speed
+		print e_type, time, radius, angle, speed
 
 def circle(time, n, radius, angle, speed):
 	arc(time, n, radius, radius, angle, angle + 360.0 * (n - 1) / n, speed)
